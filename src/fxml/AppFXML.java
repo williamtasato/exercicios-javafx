3aa7fd52a7d16778624af94d63f25d30f500f4f1
William Tasato <wg-tasato@bol.com.br>
2020-08-11 17:45:58 -0300

    tela login

--- src/fxml/AppFXML.java
+++ src/fxml/AppFXML.java
@@ -1,13 +1,25 @@
 package fxml;
 
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class AppFXML extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		URL arquivoFXML = getClass().getResource("/fxml/Login.fxml");
+		GridPane raiz =FXMLLoader.load(arquivoFXML);
+		
+		Scene cena = new Scene(raiz,350,400);
+		
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Tela de Login");
+		primaryStage.setScene(cena);
 		primaryStage.show();
 	}
 	
